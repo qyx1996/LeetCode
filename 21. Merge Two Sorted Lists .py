ccc554bf083f138ac112ada77c8c44e18e1e91be
qyx1996 <1187292885@qq.com>
2021-03-01 19:49:14 +0800

    2021.03.01 updated No. 21 and No. 494

--- 21. Merge Two Sorted Lists .py
+++ 21. Merge Two Sorted Lists .py
@@ -15,3 +15,24 @@ class Solution:
         if not l2:
             n.next = l1
         return root.next
+
+class Solution:
+    # This is my original solution. But I think it's wrong since I
+    # create new nodes, not using the originall l1 and l2 nodes
+    def mergeTwoLists(self, l1, l2):
+        root = n = ListNode(0)
+        while l1 or l2:
+            if not l1:
+                n.next = ListNode(l2.val)
+                l2 = l2.next
+            elif not l2:
+                n.next = ListNode(l1.val)
+                l1 = l1.next
+            elif l1.val >= l2.val:
+                n.next = ListNode(l2.val)
+                l2 = l2.next
+            elif l1.val < l2.val:
+                n.next = ListNode(l1.val)
+                l1 = l1.next
+            n = n.next
+        return root.next
--- 494. Target Sum.py
+++ 494. Target Sum.py
@@ -1,16 +1,37 @@
+# recursion (DP without memorization)
 class Solution:
     def findTargetSumWays(self, nums, S):
-        if nums:
-            num_equal_absS = 0
-            if len(nums) == 1:
-                if nums == S or nums == S * -1:
-                    num_equal_absS = 1
-            else:
-                return self.findTargetSumWays(nums[1: -1], S - nums[0]) + \
-                       self.findTargetSumWays(nums[1: -1], S + nums[0]) + \
-                       num_equal_absS
+        if not nums:
+            return 0
+        num_equal_absS = 0
+        if len(nums) == 1 and (nums[0] == S or nums[0] == S * -1):
+                num_equal_absS = 2 if S == 0 else 1
+        return self.findTargetSumWays(nums[1:], S - nums[0]) + self.findTargetSumWays(nums[1:], S + nums[0]) + num_equal_absS
+
+# DP with memorization
+class Solution:
+    def findTargetSumWays(self, nums, S):
+        index = len(nums) - 1
+        curr_sum = 0
+        self.memo = {}
+        return self.dp(nums, S, index, curr_sum)
+
+    def dp(self, nums, target, index, curr_sum):
+        if (index, curr_sum) in self.memo:
+            return self.memo[(index, curr_sum)]
+
+        if index < 0 and curr_sum == target:
+            return 1
+        if index < 0:
+            return 0
+
+        positive = self.dp(nums, target, index - 1, curr_sum + nums[index])
+        negative = self.dp(nums, target, index - 1, curr_sum + -nums[index])
+
+        self.memo[(index, curr_sum)] = positive + negative
+        return self.memo[(index, curr_sum)]
 
 A = Solution()
-nums = [1,1,1,1,1]
-S = 3
+nums = [25,29,23,21,45,36,16,35,13,39,44,15,16,14,45,23,50,43,9,15]
+S = 32
 print(A.findTargetSumWays(nums, S))
